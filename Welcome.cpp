55227f95f30938c223fd2c9c5af5991599138b7f
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-07-28 17:09:54 +0530

    Welcome Students To DSA 101

---
+++ Welcome.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+class Welcome 
+{
+Welcome()
+{
+  ios_base::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+}
+public:
+    std::string ToDataStructure(std::string X) 
+    {
+        return X;
+    }
+};
+
+int main()
+{
+  Welcome welcome;
+  std::cout<<"Welcome To Data Structure And Algorithm 101 Mr. "<<welcome.ToDataStructure("NAME");
+  return NULL;
+}
